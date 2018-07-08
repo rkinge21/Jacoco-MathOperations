7814e77e06bf6ad678a9fd0cb4dca050a2fb8b62
Captain <Captain@10.0.75.1>
2018-07-08 20:27:30 +0530

    Jacoco-MathOperations - Upload Project

---
+++ src/main/java/math/operation/ArithmeticOperations.java
@@ -0,0 +1,24 @@
+package math.operation;
+
+public class ArithmeticOperations {
+
+	public Integer add(Integer a, Integer b)
+	{
+		return a+b;
+	}
+
+	public Integer sub(Integer a, Integer b)
+	{
+		return a-b;
+	}
+	
+	public Integer mul(Integer a, Integer b)
+	{
+		return a*b;
+	}
+	
+	public Integer div(Integer a, Integer b)
+	{
+		return a/b;
+	}
+}
---
+++ src/test/java/math/operation/ArithmeticOperationsTest.java
@@ -0,0 +1,30 @@
+package math.operation;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ArithmeticOperationsTest 
+{
+	@Test
+	public void testAdd()
+	{
+	
+		ArithmeticOperations operations = new ArithmeticOperations();
+		Integer actual = operations.add(2, 6);
+		Integer expected = 8;
+		assertEquals(expected, actual);		
+	}
+	
+	@Test
+	public void testSub()
+	{
+	
+		ArithmeticOperations operations = new ArithmeticOperations();
+		Integer actual = operations.sub(6, 2);
+		Integer expected = 4;
+		assertEquals(expected, actual);
+		
+	}
+
+}
